35999192e40d69685b8440b9d7c371e150bde536
Szunyogg <steven.sboda@gmail.com>
2022-05-11 17:18:33 +0200

    skyrim1

---
+++ Program.cs
@@ -0,0 +1,433 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Skyrim";
+
+
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Loading");
+Console.WriteLine(".");
+Console.WriteLine("..");
+Console.WriteLine("...");
+Console.WriteLine("Swords swing faster than axes, and axes swing faster than maces.");
+Console.WriteLine("Press enter to begin your journey!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("'A carriage rumbles underneath you'");
+Console.WriteLine("'Chains bind your hands.'");
+Console.WriteLine("'Light hits your eyes and you awake'");
+Console.ReadKey();
+Console.WriteLine(" ");
+
+//Console.WriteLine("");
+
+Console.WriteLine("'A burly nordic-looking man glances at you'");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Hey\nYou\nYou're finally awake.");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("\nYou were trying to cross the border right?");
+Console.WriteLine("Walked right into that imperial ambush!\nSame as us...\nand that THIEF over there");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Damn you StormCloaks.");
+Console.WriteLine("Skyrim was fine, until you came along.\nThe Empire was nice and lazy");
+Console.WriteLine("If they hadn't been looking for you,\nI could have stolen that horse and been halfway to Hammerfell");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("\n'The Thief looks at you'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("You there!");
+Console.WriteLine("You and me. We shouldn't be here!");
+Console.WriteLine("It's these StormCloacks the Empire wants.");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\nWe're all brothers and sisters in binds now, Thief.");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nShut up back there!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The man driving the carriage says'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Whats wrong with him huh?");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Thief says, looking at the Cloaked Figure'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Wathc your tongue!\nYou're speaking to Ulfric StormCloak!\nThe TRUE High King!!!");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nUlfric?\nThe Jarl of Windhelm?\nYou're the leader of the Rebellion!");
+Console.WriteLine("If they Captured you? By the Gods, where are they taking us?!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Thief exclaims, scared senseless'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("I don't know where we're going, but\nSovngarde awaits!");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nNo!\nThis can't be happening!\nThis ISN'T happening!");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\nHey,\nWhat village are you from, Horse Thief?");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nWhy do YOU care?");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The thief looks puzzled'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("A Nord's last thoughts should be of home...");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Rorikstead.\nI'm from Rorikstead.");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You've reached the entrance of a small village'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("General Talius,sir.\nThe henchman is waiting!");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nGood!\nLet's get this over with!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Thief begins to mumble below his breath'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Azura!\nAmara!\nDibella!\nKinereth!\nAkatosh!\n");
+Console.WriteLine("Devines!\nPlease help me!\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You've reached the center of the village'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Look at him!\nGeneral Talius, the military governor...\nand it looks like the Thalmor are with him\n\nDamn Elves!");
+Console.WriteLine("I bet they had something to do with this.");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The carriage rolls on into the main square'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("This is Helgen.\n\nI used to be sweet on a girl from here.");
+Console.WriteLine("I wonder if Ilod is still making that mead with juniper berries laced in.");
+Console.WriteLine("Funny.\nWhen I was a boy, Imperial walls and towers used to make me feel so safe...");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nWho are they Daddy?");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'An onlooking boy exclaims'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("You need to go inside son!");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nWhy?\nI want to watch the soldiers!");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nInside the house!\nNow!");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nYes Papa...");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Why are we stopping?\n");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Why do you think?\nEnd of the line.");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The carriage comes to a halt'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Let's go!\nWe shouldn't keep the guards waiting for us.");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nNo!\nWait!\n\nWe're not rebels!\n");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Face your death with some courage\nThief!");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nYou've got to tell them!\nWe're with you!\nThis is a mistake!\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You and your captured fellows get off the carriage'\n");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Step towards the block when we call your name!\nOne at a time!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Ahh.\nThe Empire loves their damned lists!");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nUlfric Stormcloak,\nJarl of Windhelm.\n");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("It has been an honour,\n...Jarl Ulfric!\n");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Ralof of Riverwood!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("Lokir of Rorikstead!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("No'\nI'm not a rebel!\nYou can't do this!\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Thief makes a run for it'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Halt!\n");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("You're not gonna kill me!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Archers!\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The archers of the Empire rain down their arrows on Lokir,the Horse Thief'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Anyone else feel like running?");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Wait\nYou there!\nStep forawrd!\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You begrudgingly step forward at his command'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Who are you?\n");
+
+//  Character Creation
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'What is your name?'");
+string playerName = Console.ReadLine();
+Console.WriteLine("'What is your race?'");
+Console.WriteLine("'Nord, Orc, Dark Elf or Khajit?'");
+string playerRace = Console.ReadLine();
+Console.WriteLine("'What is your gender?'");
+Console.WriteLine("'Male or Female?'");
+string playerGender = Console.ReadLine();
+//
+
+// If statement on playerRace
+if (playerRace == "Nord")
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("I am sorry it had to come to this.");
+}
+else if (playerRace == "Orc")
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Filthy Orc!\nNever should've come here!");
+}
+else if (playerRace == "Dark Elf")
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("It's not your lucky day Elf!\nPray that the Thalmor will be merciful to you!");
+}
+else if (playerRace == "Khajit")
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("You with one of the Trade Caravans, Khajit?");
+    Console.ReadKey();
+    Console.WriteLine(" ");
+    Console.WriteLine("Your kind always seems to find trouble.");
+}
+//
+
+// If statement on playerHome
+
+string playerHome = ("");
+
+if (playerRace == "Nord")
+{
+    playerHome = ("Skyrim");
+}
+else if (playerRace == "Orc")
+{
+    playerHome = ("Orsimer");
+}
+else if (playerRace == "Dark Elf")
+{
+    playerHome = ("Morrowind");
+}
+else if (playerRace == "Khajit")
+{
+    playerHome = ("Elsweyr");
+}
+//
+
+
+
+
+
+
+string playerPronoun1 = ("");
+string playerPronoun2 = ("");
+string playerPronoun3 = ("");
+string playerPronoun4 = ("");
+
+//if statement on gender
+if (playerGender == "Male")
+{
+    playerPronoun1 = ("he's");
+    playerPronoun2 = ("he");
+    playerPronoun3 = ("him");
+    playerPronoun4 = ("his");
+
+}
+else if (playerGender == "Female")
+{
+    playerPronoun1 = ("she's");
+    playerPronoun2 = ("she");
+    playerPronoun3 = ("her");
+    playerPronoun4 = ("hers");
+}
+
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Legate looks at his superior'");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Captain!\nWhat should we do, " + playerPronoun1+ " not on the list!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Forget the list, " + playerPronoun2 + " goes to the block!");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("By your orders, Captain!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Legate turns to you'");
+
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+
+
+// if + else statement for hometext
+if  (playerRace == "Nord")
+{
+    Console.WriteLine("Im sorry.\n We'll make sure your remains are given to your family!");
+}
+else
+{
+    Console.WriteLine("I'm sorry.\nWe'll make sure that your remains are returned to " + playerHome + "!");
+}
+//
+
+
+
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You take a few steps behind the Captain'\n'and towards the block'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("Ulfric Stormcloak\nSome here in Helgen call you a hero.");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("But a hero doesn't use a power like the Voice,\nto murder his king and usurp his throne.");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("You started this war.\nRan Skyrim into chaos!\nAnd now, the Empire is going to put you down and restore the peace!");
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'Something shrieks, high up in the sky'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("What was that?\n");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("It's nothing.\nCarry on!\n");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Yes General Talius!\nGive them their last writes");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Captain turns to a Priestess of the Mara'");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("As we commend your souls to Atherius,\nBlessings of the Eight Divines upon you!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'A Nord prisoner frustratedly interrupts the Priestess'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("For the love of Talos!\nShut up!\nAnd let's get this over with!");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("\nAs you wish");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nCome on! I haven't got all Morning!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("My Ancestors are smiling at me Imperials!\nCan you say the same?");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The Nord separates from his head\n'");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("You Imperial bastards!\nJustice!\nDeath to the StormCloaks!");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'The townsfolk cry out'");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("As fearless in death\nas he was in life...");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Next!\nthe " + playerRace + "!");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'Something high up shrieks again'");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Here it is again!\nDid you hear that?");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I said!\n Next\nPrisoner\n!!!");
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("To the block, prisoner.\nNice and easy.");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("'You step up to the block'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("'You place your neck on the tree stump'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("'Something shrieks again, getting closer and closer'");
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("What in Oblivion is that?!");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nSentries\nwhat do you see?");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.ReadKey();
+Console.WriteLine(" ");
+Console.WriteLine("\n\nIT'S A DRAGON!");
+Console.WriteLine("\n\n\n");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Thank you for playing\nThe Elderscrolls V\nSkyrim on the Terminal!");
+Console.WriteLine("I hope you had fun!");
+
+//just for funsies
+if (playerGender == "Male")
+{
+    Console.WriteLine("You fucking " + playerHome + "-living " + playerRace + " dickhead!");
+}
+else if (playerGender == "Female")
+{
+    Console.WriteLine("You fucking " + playerHome + "-living " + playerRace + " cunt!");
+}
+
+
+
+
+//string race1 ="Human";
+
